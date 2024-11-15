2aedfa98644f4c694874497b80417146c7db3016
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-11-14 21:20:35 -0500

    template inicial para el header

---
+++ portafolio/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "../styles/header.css";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="title">MyLogo</div>
+      <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
