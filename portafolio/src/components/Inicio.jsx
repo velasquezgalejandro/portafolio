f5835d2ac94089832059a66273a6c469f392c08a
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-11-17 00:45:41 -0500

    avances en inicio, se crean botones y funcionalidad para descargar pdf

--- portafolio/src/components/Inicio.jsx
+++ portafolio/src/components/Inicio.jsx
@@ -1,15 +1,67 @@
 import React from "react";
+
+// icons
+import { FaDownload } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
 import "../styles/inicio.css";
 
 const Inicio = ({ isDarkTheme }) => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/assests/pdfs/dummy.pdf";
+    link.download = "dummy.pdf";
+    link.click();
+  };
+
+  const renderIconButton = (icon) => {
+    return (
+      <button
+        className={`boton-redes ${
+          isDarkTheme ? "boton-redes-dark" : "boton-redes-light"
+        }`}
+      >
+        {icon}
+      </button>
+    );
+  };
+
   return (
     <div
       className={`inicio-container ${
         isDarkTheme ? "container-dark" : "container-light"
       }`}
     >
-      <div>nombre y descripcion</div>
-      <div>Imagen</div>
+      <div className={`titulo-container`}>
+        <p className={`titulo ${isDarkTheme ? "titulo-dark" : "titulo-light"}`}>
+          Alejandro V. Dev
+        </p>
+        <p
+          className={`descripcion ${
+            isDarkTheme ? "descripcion-dark" : "descripcion-light"
+          }`}
+        >
+          Mi enfoque es crear soluciones creativas y efectivas para ayudarte a
+          destacar. Explora mi trabajo y descubre cómo puedo ayudarte a llevar
+          tu proyecto al siguiente nivel.
+        </p>
+        <div className={"botones-container"}>
+          <button
+            onClick={handleDownload}
+            className={`boton-curriculum ${
+              isDarkTheme ? "boton-curriculum-dark" : "boton-curriculum-light"
+            }`}
+          >
+            curriculum <FaDownload />
+          </button>
+          {renderIconButton(<FaGithub />)}
+          {renderIconButton(<FaXTwitter />)}
+          {renderIconButton(<FaLinkedin />)}
+        </div>
+      </div>
+      <div className={`imagen-container`}>Imagen</div>
     </div>
   );
 };
